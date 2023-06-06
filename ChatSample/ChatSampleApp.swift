7ecb608424599418579e352f72aa787f36656277
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-06-06 12:43:53 +0530

    Initial Commit

---
+++ ChatSample/ChatSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatSampleApp.swift
+//  ChatSample
+//
+//  Created by Bhumika Patel on 06/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
